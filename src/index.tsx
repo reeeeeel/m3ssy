e10509074748a739e4a6e0a13f9d6b9515d061d6
Wythh <88887043+WYTHH@users.noreply.github.com>
2022-11-21 10:11:06 +0700

    .\.editorconfig

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
+// PAGES
+import App from "./pages/App";
+
+// CSS
+import "bulma/css/bulma.css";
+import "./styles/index.scss";
+
+ReactDOM.render(
+	<Router>
+		<Routes>
+			<Route path="/" element={<App />} />
+		</Routes>
+	</Router>,
+	document.getElementById("root")
+);
